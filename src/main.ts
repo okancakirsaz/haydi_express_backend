271bc47ec06fb9e7e6a100a19560156fa0c0e18c
okancakirsaz <ocakirsaz@gmail.com>
2024-04-26 02:51:23 +0300

    cors configured

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   FirebaseServices.instance.initApp();
+
+
+  //TODO:Deactive on release
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
