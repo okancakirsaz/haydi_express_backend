e7fba4336ce42e4f140005200cc90e5fec572144
okancakirsaz <ocakirsaz@gmail.com>
2024-05-28 15:52:09 +0300

    bugs fixed

--- src/features/search/search.service.ts
+++ src/features/search/search.service.ts
@@ -5,17 +5,18 @@ import { FirebaseColumns } from 'src/core/constants/firebase_columns';
 import { BoostRestaurantOrMenuDto } from 'src/core/dt_objects/advertisement/boost_restaurant_or_menu.dto';
 import { MenuDto } from 'src/core/dt_objects/menu/menu.dto';
 import { AdvertisementCategories } from 'src/core/constants/adversiment_categories';
+import { RestaurantDto } from 'src/core/dt_objects/user/restaurant.dto';
 
 @Injectable()
 export class SearchService extends BaseService{
 
    async getSearchAds():Promise<SuggestionDto[]>{
     let suggestionList:SuggestionDto[] = [];
-    
+
     const dbDataRestaurants:BoostRestaurantOrMenuDto[] = (await this.firebase
     .getDataWithWhereQuery(FirebaseColumns.BOOSTED_RESTAURANTS,"boostArea","==",AdvertisementCategories.suggestions)??[])
     .map((e)=>BoostRestaurantOrMenuDto.fromJson(e));
-    console.log(dbDataRestaurants) 
+
     const dbDataMenu:BoostRestaurantOrMenuDto[] = (await this.firebase
         .getDataWithWhereQuery(FirebaseColumns.BOOSTED_MENUS,"boostArea","==",AdvertisementCategories.suggestions)??[])
         .map((e)=>BoostRestaurantOrMenuDto.fromJson(e));
@@ -26,6 +27,7 @@ export class SearchService extends BaseService{
     suggestionList.push(await this.fetchSuggestion(dbData[i]));
     }
     return suggestionList;
+    
    } 
 
    private async fetchSuggestion(data:BoostRestaurantOrMenuDto):Promise<SuggestionDto>{
@@ -37,10 +39,16 @@ export class SearchService extends BaseService{
     if(!data.isRestaurant){
      const menu:MenuDto = MenuDto.fromJson(await this.firebase.getDoc(FirebaseColumns.RESTAURANT_MENUS,suggestion.elementId));
      suggestion.isOnDiscount=menu.isOnDiscount;
+     suggestion.name=menu.name;
      suggestion.discountAmount=menu.discountAmount??0;
+     suggestion.price=menu.price;
     }
     else{
+        const restaurant:RestaurantDto = 
+        RestaurantDto.fromJson(await this.firebase.getDoc(FirebaseColumns.RESTAURANTS,suggestion.elementId));
+        suggestion.name= restaurant.businessName
         suggestion.isOnDiscount=false;
+        suggestion.price=0;
         suggestion.discountAmount=0;
     }
     return suggestion;
