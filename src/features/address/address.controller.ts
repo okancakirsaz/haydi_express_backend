ac1fbb233001857c4a9a8931469707a3c5c09755
okancakirsaz <ocakirsaz@gmail.com>
2024-06-07 13:12:58 +0300

    yandex deprecated

--- src/features/address/address.controller.ts
+++ src/features/address/address.controller.ts
@@ -6,10 +6,4 @@ import { AuthGuard } from 'src/core/guard/auth.guard';
 export class AddressController{
 constructor(private readonly service:AddressService){}
 
-
-@UseGuards(AuthGuard)
-@Get("map-kit")
-getApiKey(){
-    return "14ed6c2b-adf0-4449-959b-62ca9708f445";
-}
 }
